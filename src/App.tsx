8766eaa79f009706f8f3871e6730d4ffd0ff2295
Bovt Dmytro <mr.bovt2004@gmail.com>
2024-12-17 21:49:32 +0200

    done task

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,20 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
 
 export const App: React.FC = () => {
+  const [isLoad, setIsLoad] = useState(true);
+  const [isModalSee, setIsModalSee] = useState(false);
+  const [todos, setTodos] = useState<Todo[] | null>(null);
+  const [todo, setTodo] = useState<Todo | null>(null);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoad(false);
+    }, 1000);
+  }, [isLoad]);
+
   return (
     <>
       <div className="section">
@@ -17,18 +29,26 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter setTodos={setTodos} />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoad ? (
+                <Loader />
+              ) : (
+                <TodoList
+                  setModalVisible={setIsModalSee}
+                  setSelectedTodo={setTodo}
+                  todos={todos}
+                  selectedTodo={todo}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {isModalSee && <TodoModal setVisible={setIsModalSee} todo={todo} />}
     </>
   );
 };
